45f9d1eb42c22f674f2c3e1e62f864c5b9e3fb24
Mia Cabrera <antemia.kavillari@gmail.com>
2021-04-25 16:04:31 -0500

    added loading spinner

--- index.js
+++ index.js
@@ -16,20 +16,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-// async function fetchSenate() {
-//   let response = await fetch('https://senate-stock-watcher-data.s3-us-west-2.amazonaws.com/')
-//   .then((response) => response.text())
-//   .then((response) => {
-//     const parser = new DOMParser()
-//     const xml = parser.parseFromString(response, 'text/xml')
-//     const results = [].slice.call( xml.getElementsByTagName('Key') ).filter((key) => key.textContent.includes('.json'))
-//     const files = results.map(file => file.textContent.split('/')[1])})
-
-//     fetchSenate()
-//       .catch((response) => {
-//       console.log(response)
-// })}; 
-
 app.get('/', function (req, res) {
     res.json(all_transactions);
     
--- senate-script.js
+++ senate-script.js
@@ -1,20 +1,23 @@
 const api = 'http://localhost:4000';
+const axios = require('axios'); 
 
 async function getSenators(api) {
-    const response = await fetch(api);
-
-    let data = await response.json();
+    const response = await axios.get(api);
+    let data = JSON.stringify(response);
     console.log(data);
     if (response) {
-        hideloader();
-    }
+        showloader();
+    } else {
     show(data)
+    }
 }; 
 
 getSenators(api);
 
-function hideloader() {
-document.getElementById('loader').style.display = 'none'
+function showloader() {
+    let spinner = document.getElementById('loading');
+    console.log("loading senate stock data")
+    spinner.style.display = 'block';
 }
 
 function show(data) {
@@ -27,12 +30,13 @@ function show(data) {
          </tr>`;
     
     // Loop to access all rows 
-    for (let r of data.list) {
-        table += `<tr> 
-    <td>${r.senator} </td>
-    <td>${r.asset_description}</td>
-    <td>${r.amount}</td> 
-    <td>${r.transaction_date}</td>          
+    for (let r of data.json) {
+        table += 
+        `<tr> 
+        <td>${r.senator} </td>
+        <td>${r.asset_description}</td>
+        <td>${r.amount}</td> 
+        <td>${r.transaction_date}</td>          
 </tr>`;
     }
     // Setting innerHTML as tab variable
